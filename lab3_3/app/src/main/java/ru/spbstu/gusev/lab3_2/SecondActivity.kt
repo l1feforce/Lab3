86e2e2fefb934d2eaac4283dbf5639e8a27b7ffd
l1feforce <31709474+l1feforce@users.noreply.github.com>
2019-10-26 01:53:01 +0300

    Add files via upload

---
+++ lab3_3/app/src/main/java/ru/spbstu/gusev/lab3_2/SecondActivity.kt
@@ -0,0 +1,37 @@
+package ru.spbstu.gusev.lab3_2
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.layout_first.*
+import kotlinx.android.synthetic.main.layout_second.*
+
+class SecondActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        val navView: NavigationView = findViewById(R.id.nav_view)
+        val toggle = ActionBarDrawerToggle(
+            this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close
+        )
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        navView.setNavigationItemSelectedListener(this)
+
+        to_first_button.setOnClickListener {
+            finish()
+        }
+        to_third_button.setOnClickListener {
+            startActivity(Intent(this, ThirdActivity::class.java))
+        }
+    }
+}
